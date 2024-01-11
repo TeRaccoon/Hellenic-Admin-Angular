f687b5edd8b49f5acd8402aedd9c2bb0c958f31e
TeRaccoon <jack.sam.benning@gmail.com>
2024-01-11 21:58:58 +0000

    Feature: Added rough addition of data to invoice

--- src/app/components/invoice-view/invoice-view.component.ts
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invoice-view',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./invoice-view.component.scss']
 })
 export class InvoiceViewComponent {
+  invoiceData: any[] = [];
+  invoiceItems: any[] = [];
+  date = new Date();
 
+  constructor(private router: Router, private dataService: DataService) {}
+
+  ngOnInit() {
+    this.getInvoiceData();
+  }
+
+  getInvoiceData() {
+    var invoiceIds = this.dataService.retrievePrintInvoiceIds();
+    if (invoiceIds.length == 0) {
+      this.router.navigate(['/view'], { queryParams: {table: 'invoices'}});
+    }
+    invoiceIds.forEach(invoiceId => {
+      this.dataService.collectData("invoice-info", invoiceId.toString()).subscribe((data: any) => {
+        this.invoiceData.push(data);
+      });
+      this.dataService.collectData("invoice-products", invoiceId.toString()).subscribe((data: any) => {
+        this.invoiceItems.push(data);
+        console.log(this.invoiceItems);
+      });
+    });
+  }
 }
