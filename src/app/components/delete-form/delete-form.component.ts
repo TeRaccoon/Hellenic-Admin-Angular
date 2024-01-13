0fb90d5d149114cb0c77c979a7ed8e6bb434dfa3
TeRaccoon <jack.sam.benning@gmail.com>
2024-01-13 00:22:54 +0000

    Feature: Added rest of delete functionality

--- src/app/components/delete-form/delete-form.component.ts
+++ src/app/components/delete-form/delete-form.component.ts
@@ -10,6 +10,7 @@ import { FormService } from '../../services/form.service';
 export class DeleteFormComponent {
   formVisible = 'hidden';
   ids: string[] = [];
+  tableName: string = "";
 
   constructor(private dataService: DataService, private formService: FormService) {  }
 
@@ -17,10 +18,27 @@ export class DeleteFormComponent {
     this.formService.getDeleteFormVisibility().subscribe((visible) => {
       this.formVisible = visible ? 'visible' : 'hidden';
       this.ids = this.formService.getDeleteFormIds();
+      this.tableName = this.formService.getSelectedTable();
     });
   }
 
   hide() {
     this.formService.hideDeleteForm();
+  }
+
+  deleteRow() {
+    var idString = "";
+    if (this.ids.length > 1) {
+      this.ids.forEach(id => {
+        idString += id + ", ";
+      });
+      idString = idString.substring(0, idString.length - 2);
+    }
+    else {
+      idString = this.ids[0];
+    }
+    this.dataService.submitFormData({action: 'delete', id: idString, table_name: this.tableName}).subscribe((data: any) => {
+
+    });
   }
 }
