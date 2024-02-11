dd00defb4f0ecd650b9d00cb9e48b7f62b0188e0
jessiemay00 <jesswhitaker119@outlook.com>
2024-02-11 21:26:03 +0000

    Design: filter error

--- src/app/components/filter-form/filter-form.component.ts
+++ src/app/components/filter-form/filter-form.component.ts
@@ -52,7 +52,7 @@ export class FilterFormComponent {
       this.hide();
       this.resetForm();      
     } else {
-      this.errorMsg = "Please fill in all required fields!";
+      this.errorMsg = "Please fill in all required fields";
     }
   }
 
