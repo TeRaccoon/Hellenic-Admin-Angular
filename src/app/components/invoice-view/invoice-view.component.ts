b1cc2e7f442b42b1227c9320ec252bcb4e9a657b
TeRaccoon <jack.sam.benning@gmail.com>
2024-02-29 18:35:49 +0000

    Feature: Fixed loading issue and added delivery info table skeleton at bottom of page

--- src/app/components/invoice-view/invoice-view.component.ts
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
 import { urlBase } from '../../services/data.service';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-invoice-view',
@@ -11,34 +13,51 @@ import { urlBase } from '../../services/data.service';
 export class InvoiceViewComponent {
   invoiceData: any[] = [];
   invoiceItems: any[] = [];
+  deliveryData: any[] = [];
+  customerIds: any[] = [];
+
+  faSpinner = faSpinner;
+
   date = new Date();
   urlBase = urlBase;
+
+  loaded = false;
 
   constructor(private router: Router, private dataService: DataService) {}
 
   ngOnInit() {
+    this.loaded = false;
     this.getInvoiceData();
   }
 
-  getInvoiceData() {
+  async getInvoiceData() {
     var invoiceIds = this.dataService.retrievePrintInvoiceIds();
     if (invoiceIds.length == 0) {
       this.router.navigate(['/view'], { queryParams: {table: 'invoices'}});
+      return;
     }
-    invoiceIds.forEach(invoiceId => {
-      this.dataService.collectData("invoice-info", invoiceId.toString()).subscribe((data: any) => {
-        this.invoiceData.push(data);
-      });
-      this.dataService.collectData("invoice-products", invoiceId.toString()).subscribe((data: any) => {
-        this.invoiceItems.push(Array.isArray(data) ? data : [data]);
-        this.calculateVat();
-      });
-    });
+    for (const invoiceId of invoiceIds) {
+      const invoiceData: any = await lastValueFrom(this.dataService.collectData("invoice-info", invoiceId.toString()));
+      this.invoiceData.push(invoiceData);
+      
+      const productData: any = await lastValueFrom(this.dataService.collectData("invoice-products", invoiceId.toString()));
+      this.invoiceItems.push(Array.isArray(productData) ? productData : [productData]);
+      
+      const deliveryData: any = await lastValueFrom(this.dataService.collectData("delivery-info", invoiceId.toString()));
+      if (this.customerIds.indexOf(deliveryData.customer_id) == -1) {
+        this.deliveryData.push(deliveryData);
+        this.customerIds.push(deliveryData.customer_id);
+      }
+    }
+    this.calculateVat();
+    this.loaded = true;
   }
 
   calculateVat() {
+    console.log(this.invoiceItems);
     this.invoiceData.forEach((invoices: any, index) => {
       var vatTotal = 0;
+
       this.invoiceItems[index].forEach((item: any) => {
         if (item['vat_charge'] == 'Yes') {
           var vatAmount = item['quantity'] * item['price'] * 0.2;
