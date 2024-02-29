a2352cdc076e8ddf445a8f3020b1385bdd138c05
TeRaccoon <jack.sam.benning@gmail.com>
2024-02-29 22:07:09 +0000

    Feature: Added table showing delivery information for driver at the bottom of the invoice view

--- src/app/components/invoice-view/invoice-view.component.ts
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -43,21 +43,42 @@ export class InvoiceViewComponent {
       const productData: any = await lastValueFrom(this.dataService.collectData("invoice-products", invoiceId.toString()));
       this.invoiceItems.push(Array.isArray(productData) ? productData : [productData]);
       
-      const deliveryData: any = await lastValueFrom(this.dataService.collectData("delivery-info", invoiceId.toString()));
+      var deliveryData: any = await lastValueFrom(this.dataService.collectData("delivery-info", invoiceId.toString()));
+      deliveryData = this.calculateDistance(deliveryData);
       if (this.customerIds.indexOf(deliveryData.customer_id) == -1) {
         this.deliveryData.push(deliveryData);
         this.customerIds.push(deliveryData.customer_id);
       }
     }
+    console.log(this.deliveryData);
     this.calculateVat();
     this.loaded = true;
   }
 
+  calculateDistance(deliveryData: any) {
+    deliveryData['distance'] = this.calculateHaversine(deliveryData.customer_coordinates, deliveryData.warehouse_coordinates);
+    return deliveryData
+  }
+
+  calculateHaversine(coord1: any, coord2: any): string {
+    const R = 6371000; // Earth's radius in meters
+    const lat1Rad = coord1.latitude * (Math.PI/180);
+    const lat2Rad = coord2.latitude * (Math.PI/180);
+    const deltaLat = (coord2.latitude - coord1.latitude) * (Math.PI/180);
+    const deltaLon = (coord2.longitude - coord1.longitude) * (Math.PI/180);
+  
+    const a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+              Math.cos(lat1Rad) * Math.cos(lat2Rad) *
+              Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  
+    const distance = R * c / 1000; // Distance in km
+    return distance.toFixed(2);
+  }
+
   calculateVat() {
-    console.log(this.invoiceItems);
     this.invoiceData.forEach((invoices: any, index) => {
       var vatTotal = 0;
-
       this.invoiceItems[index].forEach((item: any) => {
         if (item['vat_charge'] == 'Yes') {
           var vatAmount = item['quantity'] * item['price'] * 0.2;
