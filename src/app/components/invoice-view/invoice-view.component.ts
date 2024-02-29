ffc27c9d6b7cdce245d27f03e1dbce94d0e5f6c7
TeRaccoon <jack.sam.benning@gmail.com>
2024-02-29 12:27:15 +0000

    Refactor: Fixed and refactored invoice view and values

--- src/app/components/invoice-view/invoice-view.component.ts
+++ src/app/components/invoice-view/invoice-view.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
+import { urlBase } from '../../services/data.service';
 
 @Component({
   selector: 'app-invoice-view',
@@ -11,6 +12,7 @@ export class InvoiceViewComponent {
   invoiceData: any[] = [];
   invoiceItems: any[] = [];
   date = new Date();
+  urlBase = urlBase;
 
   constructor(private router: Router, private dataService: DataService) {}
 
@@ -29,7 +31,24 @@ export class InvoiceViewComponent {
       });
       this.dataService.collectData("invoice-products", invoiceId.toString()).subscribe((data: any) => {
         this.invoiceItems.push(Array.isArray(data) ? data : [data]);
+        this.calculateVat();
       });
+    });
+  }
+
+  calculateVat() {
+    this.invoiceData.forEach((invoices: any, index) => {
+      var vatTotal = 0;
+      this.invoiceItems[index].forEach((item: any) => {
+        if (item['vat_charge'] == 'Yes') {
+          var vatAmount = item['quantity'] * item['price'] * 0.2;
+          item['vat_charge'] = vatAmount;
+          vatTotal += vatAmount;
+        } else {
+          item['vat_charge'] = 0;
+        }
+      });
+      invoices['vat'] = vatTotal;
     });
   }
 }
