0103b000dbcc20bbf658b82ab770017916c47b74
TeRaccoon <jack.sam.benning@gmail.com>
2024-02-29 12:27:35 +0000

    Feature: Added print excluded back button to print preview

--- src/app/components/print-layout/print-layout.component.ts
+++ src/app/components/print-layout/print-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
+import { faA, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-print-layout',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./print-layout.component.scss']
 })
 export class PrintLayoutComponent {
+  faArrowLeft = faArrowLeft;
 
+  constructor(private _location: Location) {}
+
+  back() {
+    this._location.back()
+  }
 }
