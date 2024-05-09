6feceb045da5855ed6641faeebff9e2bf2ca3400
TeRaccoon <jack.sam.benning@gmail.com>
2024-05-09 23:18:09 +0100

    Feature: Added vat group selector

--- src/app/components/table-widget/table-widget.component.ts
+++ src/app/components/table-widget/table-widget.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { lastValueFrom } from 'rxjs';
-import { FormService } from '../../services/form.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -18,11 +17,14 @@ export class TableWidgetComponent {
   alternativeData: any;
   query = '';
 
+  vatReturnHistory: any[] = []
+  vatHistory: string[] = [];
+  selectedVatGroup: string = "";
+
   displayDateRange = false;
 
   constructor(
     private dataService: DataService,
-    private formService: FormService,
     private route: ActivatedRoute
   ) {}
 
@@ -36,48 +38,62 @@ export class TableWidgetComponent {
   selectTable() {
     switch (this.tableName) {
       case 'vat-returns':
-        this.headers = [
-          'Total Sales',
-          'Output VAT',
-          'Total Expenses',
-          'Input VAT',
-          'VAT Liability',
-        ];
-        this.query = 'vat-data';
-        this.displayDateRange = true;
-        this.columnTypes = [
-          'currency',
-          'currency',
-          'currency',
-          'currency',
-          'currency',
-        ];
-        this.alternativeData = {
-          text: [
-            'VAT due in this period on sales and other outputs',
-            'VAT due in this period on intra-community acquisitions of goods made in Northern Ireland from EU Member States',
-            'Total VAT due (the sum of boxes 1 and 2)',
-            'VAT reclaimed in this period on purchases and other inputs (including acquisitions in the EC)',
-            'Net VAT to be paid to HMRC or reclaimed by you (Difference between boxes 3 and 4)',
-            'Total value of sales and all other outputs excluding any VAT (Include your box 8 figure)',
-            'Total value of purchases and all other inputs excluding VAT (Include your box 9 figure)',
-            'Total value of intra-community dispatches of goods and related costs (excluding VAT) from Northern Ireland to EU Member States',
-            'Total value of intra-community acquisitions of goods and related costs (excluding VAT) made in Northern Ireland from EU Member States',
-          ],
-          altText: [
-            'VAT due and other outputs',
-            'VAT due on intra-community acquisitions of goods made in Northern Ireland from EU Member States',
-            'Total VAT due',
-            'VAT reclaimed on purchases and other inputs including EC acquisitions',
-            'Net VAT due',
-            'Total value of sales and other outputs including EC supplies',
-            'Total value of purchases and other inputs including EC acquisitions',
-            'Total value of intra-community dispatches from Northern Ireland to EU Member States',
-            'Total value of intra-community acquisitions made in Northern Ireland from EU Member States',
-          ]
-        };
+        this.loadVATData();
         break;
     }
+  }
+
+  async loadVATData() {
+    this.headers = [
+      'Total Sales',
+      'Output VAT',
+      'Total Expenses',
+      'Input VAT',
+      'VAT Liability',
+    ];
+    this.query = 'vat-data';
+    this.displayDateRange = true;
+    this.columnTypes = [
+      'currency',
+      'currency',
+      'currency',
+      'currency',
+      'currency',
+    ];
+    this.alternativeData = {
+      text: [
+        'VAT due in this period on sales and other outputs',
+        'VAT due in this period on intra-community acquisitions of goods made in Northern Ireland from EU Member States',
+        'Total VAT due (the sum of boxes 1 and 2)',
+        'VAT reclaimed in this period on purchases and other inputs (including acquisitions in the EC)',
+        'Net VAT to be paid to HMRC or reclaimed by you (Difference between boxes 3 and 4)',
+        'Total value of sales and all other outputs excluding any VAT (Include your box 8 figure)',
+        'Total value of purchases and all other inputs excluding VAT (Include your box 9 figure)',
+        'Total value of intra-community dispatches of goods and related costs (excluding VAT) from Northern Ireland to EU Member States',
+        'Total value of intra-community acquisitions of goods and related costs (excluding VAT) made in Northern Ireland from EU Member States',
+      ],
+      altText: [
+        'VAT due and other outputs',
+        'VAT due on intra-community acquisitions of goods made in Northern Ireland from EU Member States',
+        'Total VAT due',
+        'VAT reclaimed on purchases and other inputs including EC acquisitions',
+        'Net VAT due',
+        'Total value of sales and other outputs including EC supplies',
+        'Total value of purchases and other inputs including EC acquisitions',
+        'Total value of intra-community dispatches from Northern Ireland to EU Member States',
+        'Total value of intra-community acquisitions made in Northern Ireland from EU Member States',
+      ]
+    };
+
+    this.loadVATGroups()
+  }
+
+  async loadVATGroups() {
+    this.vatHistory = await lastValueFrom(this.dataService.processData('vat-groups'))
+  }
+
+  async loadVATReturns() {
+    this.vatReturnHistory = await lastValueFrom(this.dataService.processData("vat-history-by-group-id", this.selectedVatGroup));
   }
 
   async collectData() {
