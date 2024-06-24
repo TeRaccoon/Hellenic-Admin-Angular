fb54db7a1e832fc9bb488ca94f5bb3a19fc80164
TeRaccoon <jack.sam.benning@gmail.com>
2024-06-24 23:16:23 +0100

    Refactor: Delegated common types to own file

---
+++ src/app/common/types/statistics/types.ts
@@ -0,0 +1,27 @@
+import { ChartConfiguration, ChartType } from 'chart.js';
+
+export interface chart {
+    data: ChartConfiguration['data'],
+    options: ChartConfiguration['options'], 
+    type: ChartType, 
+    heading: string, 
+    subheading: string,
+    query: query,
+}
+
+export interface report {
+    data: any[],
+    headers: string[] | null,
+    dataTypes: string[],
+    type: string
+}
+
+export const REPORT_TYPE = {
+    list: 'LIST',
+    table: 'TABLE'
+};
+
+interface query {
+    dayQueries: string[],
+    monthQueries: string[],
+}
