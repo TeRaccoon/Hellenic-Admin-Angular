0c78fafe570decafcb65d458121f039455f20f9f
TeRaccoon <jack.sam.benning@gmail.com>
2024-08-27 14:04:32 +0100

    Feature: Added table categories to constants

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -43,6 +43,50 @@ export const TABLE_NAME_PAIRS = [
     { 'sub_categories': 'Sub Categories' }
 ];
 
+export const TABLE_CATEGORIES =
+{
+    "Customers": [
+        { tableName: "customers", displayName: "Overview" },
+        { tableName: "payments", displayName: "Payments" },
+        { tableName: "invoices", displayName: "Invoices" },
+        { tableName: "price_list", displayName: "Price List" }
+    ],
+    "Products": [
+        { tableName: "items", displayName: "Products" },
+    ],
+    "Supply": [
+        { tableName: "stocked_items", displayName: "Stock" },
+        { tableName: 'expired_items', displayName: 'Expired Items' },
+        { tableName: "supplier_invoices", displayName: "Invoices" },
+        { tableName: "suppliers", displayName: "Suppliers" },
+        { tableName: "warehouse", displayName: "Warehouses" },
+    ],
+    "Finance": [
+        { tableName: "invoices", displayName: "Customer Invoices" },
+        { tableName: "supplier_invoices", displayName: "Supplier Invoices" },
+        { tableName: "general_ledger", displayName: "General Ledger" },
+        { tableName: "payments", displayName: "All Payments" },
+        { tableName: "profit_loss", displayName: "Profit / Loss" },
+        { tableName: "customer_payments", displayName: "Invoice Payments" },
+        { tableName: "debtor_creditor", displayName: "Aged Debtor / Creditors" },
+        { tableName: "vat-returns", displayName: "VAT Returns" }
+    ],
+    "Website": [
+        { tableName: "offers", displayName: "Offers" },
+        { tableName: "categories", displayName: "Categories" },
+        { tableName: "sub_categories", displayName: "Sub-categories" },
+        { tableName: "statistics", displayName: "Statistics" },
+        { tableName: "retail_items", displayName: "Listed Items" },
+        { tableName: "discount_codes", displayName: "Discount codes" },
+        { tableName: "page_section_text", displayName: "Website Text" },
+        { tableName: "image_locations", displayName: "Website Images" }
+    ],
+    "Admin": [
+        { tableName: "settings", displayName: "Settings" },
+        { tableName: "users", displayName: "Users" },
+    ]
+};
+
 export const TABLE_NAME_MAP = new Map<string, string>(
     TABLE_NAME_PAIRS.map((pair: any) => {
         const key = Object.keys(pair)[0];
