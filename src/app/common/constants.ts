f00aa0b9b187a42956d0b480be9f0c40b5f0a859
TeRaccoon <jack.sam.benning@gmail.com>
2024-08-27 11:06:10 +0100

    Feature: Table name constants and mapping functions

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -20,3 +20,47 @@ export const TABLE_NAMES = [
     'categories',
     'sub_categories'
 ];
+
+export const TABLE_NAME_PAIRS = [
+    { 'customers': 'Customers' },
+    { 'invoices': 'Invoices' },
+    { 'payments': 'Payments' },
+    { 'price_list': 'Price List' },
+    { 'items': 'Items' },
+    { 'stocked_items': 'Stocked Items' },
+    { 'supplier_invoices': 'Supplier Invoices' },
+    { 'suppliers': 'Suppliers' },
+    { 'warehouse': 'Warehouse' },
+    { 'general_ledger': 'General Ledger' },
+    { 'customer_payments': 'Customer Payments' },
+    { 'discount_codes': 'Discount Codes' },
+    { 'retail_items': 'Retail Items' },
+    { 'page_section_text': 'Page Section Text' },
+    { 'image_locations': 'Image Locations' },
+    { 'page_sections': 'Page Sections' },
+    { 'offers': 'Offers' },
+    { 'categories': 'Categories' },
+    { 'sub_categories': 'Sub Categories' }
+];
+
+export const TABLE_NAME_MAP = new Map<string, string>(
+    TABLE_NAME_PAIRS.map((pair: any) => {
+        const key = Object.keys(pair)[0];
+        const value = pair[key];
+        return [key, value];
+    })
+);
+
+export const REVERSE_TABLE_NAME_MAP = new Map<string, string>(
+    TABLE_NAME_PAIRS.map((pair: any) => {
+        const key = Object.keys(pair)[0];
+        const value = pair[key];
+        return [value, key];
+    })
+);
+
+export const NON_VIEW_TABLES = [
+    'debtor_creditor',
+    'statistics',
+    'profit_loss'
+];
