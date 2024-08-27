c24d856c06abe99cf0461d24db09646c697db262
TeRaccoon <jack.sam.benning@gmail.com>
2024-08-27 11:10:41 +0100

    Refactor: Moved navbar icons to own file

---
+++ src/app/common/icons/navbar-icons.ts
@@ -0,0 +1,10 @@
+import { faBell, faEnvelope, faUser, faSearch, faFileCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+export const navbarIcons = {
+    faBell,
+    faEnvelope,
+    faUser,
+    faSearch,
+    faFileCircleXmark,
+    faSpinner,
+};
