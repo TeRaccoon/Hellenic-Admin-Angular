75ad7b362cd8320fe85abbdc0886109333b85d8a
TeRaccoon <jack.sam.benning@gmail.com>
2024-09-05 17:10:05 +0100

    HELL-158: Added extra data to search results if required and excludes certain columns from having results displayed

--- src/app/common/types/table.ts
+++ src/app/common/types/table.ts
@@ -1,10 +1,16 @@
 export interface SearchResult {
-    tableName: string,
-    id: number,
-    matchedValue: string
-};
+  tableName: string;
+  id: number;
+  matchedValue: string;
+  displayValue: string;
+}
 
 export interface Extras {
-    StockData: any,
-    Images: { [key: string]: any }[]
+  StockData: any;
+  Images: { [key: string]: any }[];
+}
+
+export const EXCLUDED_COLUMNS: Record<string, string[]> = {
+  customers: ['currency_code', 'password'],
+  suppliers: ['currency_code'],
 };
--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from '@angular/core';
 import { TABLE_NAMES } from '../common/constants';
 import { DataService } from './data.service';
-import { SearchResult } from '../common/types/table';
+import { EXCLUDED_COLUMNS, SearchResult } from '../common/types/table';
 import { TableService } from './table.service';
 
 @Injectable({
   providedIn: 'root',
 })
-
 export class SearchService {
-
-  constructor(private dataService: DataService, private tableService: TableService) { }
+  constructor(
+    private dataService: DataService,
+    private tableService: TableService
+  ) {}
 
   async search(filter: string): Promise<SearchResult[]> {
     let filteredData: SearchResult[] = [];
@@ -41,26 +42,62 @@ export class SearchService {
   }
 
   searchRow(row: any, filter: string, tableName: string) {
-    let matchedData: any[] = [];
-
+    let matchedData: SearchResult[] = [];
     Object.entries(row).forEach(([key, value]: [string, any]) => {
       if (typeof value === 'string' && value.includes(filter)) {
-        matchedData.push({
-          tableName: this.tableService.getTableDisplayName(tableName),
-          id: row['id'],
-          matchedValue: value
-        });
+        let tableDisplayName =
+          this.tableService.getTableDisplayName(tableName) ?? '';
+        let displayValue = this.processDisplayValue(tableName, value, key, row);
+
+        if (this.shouldDisplayMatch(tableName, key)) {
+          matchedData.push({
+            tableName: tableDisplayName,
+            id: row['id'],
+            matchedValue: value,
+            displayValue: displayValue,
+          });
+        }
       }
     });
 
     return matchedData.length > 0 ? matchedData : null;
   }
 
+  shouldDisplayMatch(tableName: string, key: string) {
+    return (
+      !(
+        tableName in EXCLUDED_COLUMNS &&
+        EXCLUDED_COLUMNS[tableName].includes(key)
+      ) || !(tableName in EXCLUDED_COLUMNS)
+    );
+  }
+
+  processDisplayValue(
+    tableName: string,
+    matchedValue: string,
+    key: string,
+    row: any
+  ) {
+    switch (tableName) {
+      case 'customers':
+        if (key == 'account_number') {
+          return `${row['account_name']}: ${matchedValue}`;
+        }
+        break;
+    }
+
+    return matchedValue;
+  }
+
   async getAllTableData() {
     let tableData: any[] = [];
 
     for (const table of TABLE_NAMES) {
-      let data = await this.dataService.processGet('display-data', { filter: table }, true);
+      let data = await this.dataService.processGet(
+        'display-data',
+        { filter: table },
+        true
+      );
       tableData.push(data);
     }
     return tableData;
