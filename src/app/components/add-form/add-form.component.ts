61f17212e8fcfaf2141b31085c7ae1f51108f517
TeRaccoon <jack.sam.benning@gmail.com>
2024-10-07 22:47:53 +0100

    HELL-232: Fixed issue with sales type disabled variable

--- src/app/components/add-form/add-form.component.ts
+++ src/app/components/add-form/add-form.component.ts
@@ -226,7 +226,7 @@ export class AddFormComponent {
     this.addressNotListedKeys = [];
     this.invoiceDetails = [];
     this.selectData = [];
-    this.saleTypeEnabled = false;
+    this.saleTypeEnabled = true;
   }
 
   async replaceAmbiguousData() {
@@ -599,7 +599,7 @@ export class AddFormComponent {
         this.addForm.get('table_name')?.setValue(this.tableName);
         this.formState.error = null;
         this.selectedReplacementData = {};
-        this.saleTypeEnabled;
+        this.saleTypeEnabled = true;
       }
     }
     this.formState.submitted = true;
