77c15d336e1f5f04c837e72aa0bdff8d6821ba63
TeRaccoon <jack.sam.benning@gmail.com>
2024-10-14 21:52:13 +0100

    HELL-239: Fixed empty columns in VAT return

--- src/app/components/vat-view/vat-view.component.ts
+++ src/app/components/vat-view/vat-view.component.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-vat-view',
   templateUrl: './vat-view.component.html',
-  styleUrl: './vat-view.component.scss'
+  styleUrl: './vat-view.component.scss',
 })
 export class VatViewComponent {
   vatData: any = {};
@@ -20,6 +20,14 @@ export class VatViewComponent {
     if (this.vatData.labels.length == 0) {
       this.router.navigate(['/home']);
     }
+  }
+
+  format(value: any) {
+    if (value == '' || value == null || value == undefined) {
+      return 0;
+    }
+
+    return value;
   }
 
   print() {
