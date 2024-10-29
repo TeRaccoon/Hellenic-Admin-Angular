89187da4fca9bd40184b97ae058fbf1f95eddab1
TeRaccoon <jack.sam.benning@gmail.com>
2024-10-29 09:16:11 +0000

    HELL-160: Added spinner icon

--- src/app/common/icons/chart-icons.ts
+++ src/app/common/icons/chart-icons.ts
@@ -1,11 +1,21 @@
-import { faFileContract, faX, faFilter, faArrowUpWideShort, faArrowDown19, faArrowDown91, faWarning } from '@fortawesome/free-solid-svg-icons';
+import {
+  faFileContract,
+  faX,
+  faFilter,
+  faArrowUpWideShort,
+  faArrowDown19,
+  faArrowDown91,
+  faWarning,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 export const chartIcons = {
-    faFileContract,
-    faX,
-    faFilter,
-    sort: faArrowUpWideShort,
-    sortLowToHigh: faArrowDown19,
-    sortHighToLow: faArrowDown91,
-    warning: faWarning,
+  faFileContract,
+  faX,
+  faFilter,
+  sort: faArrowUpWideShort,
+  sortLowToHigh: faArrowDown19,
+  sortHighToLow: faArrowDown91,
+  warning: faWarning,
+  spinner: faSpinner,
 };
