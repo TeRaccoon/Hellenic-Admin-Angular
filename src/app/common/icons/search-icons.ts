eb6b8e7b991014ebb6b749616f0e96d4c0f38311
TeRaccoon <jack.sam.benning@gmail.com>
2024-10-29 09:15:04 +0000

    HELL-160: Added icons for search component

---
+++ src/app/common/icons/search-icons.ts
@@ -0,0 +1,13 @@
+import {
+  faX,
+  faSearch,
+  faSpinner,
+  faFileCircleXmark,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const searchIcons = {
+  x: faX,
+  search: faSearch,
+  spinner: faSpinner,
+  noFile: faFileCircleXmark,
+};
