bbeed5eecf02abacc13a19730f12f063534ad97d
TeRaccoon <jack.sam.benning@gmail.com>
2024-11-06 21:22:52 +0000

    Amended incorrect env files

--- src/app/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  configUrl: '/assets/config/config.json',
-};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const recaptcha = {
-    production: false,
-    siteKey: '6LfXWO4pAAAAAJA_r7-7Oq5bVJ4TkSOjLtMoKVhO'
+  production: false,
+  siteKey: '6LfXWO4pAAAAAJA_r7-7Oq5bVJ4TkSOjLtMoKVhO',
+};
+
+export const environment = {
+  production: false,
+  configUrl: '/assets/config/config.json',
 };
