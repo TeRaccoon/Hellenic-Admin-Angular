762ecca0ec474c70e2699c35def14b66846fa2c2
TeRaccoon <jack.sam.benning@gmail.com>
2025-04-11 19:42:40 +0100

    HELL-432: Added from items table component

---
+++ src/app/components/form-items-table/form-items-table.component.ts
@@ -0,0 +1,82 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { faX } from '@fortawesome/free-solid-svg-icons';
+import { DataService } from '../../services/data.service';
+import { Subscription } from 'rxjs';
+import { FormService } from '../../services/form.service';
+import { ItemsList } from '../../common/types/form-items-table/types';
+
+@Component({
+  selector: 'app-form-items-table',
+  templateUrl: './form-items-table.component.html',
+  styleUrl: './form-items-table.component.scss'
+})
+export class FormItemsTableComponent {
+  @Input() tableName!: string;
+  @Input() invoiceId!: number;
+
+  private readonly subscriptions = new Subscription();
+
+  invoiceTotal: number = 0;
+  itemsList: ItemsList[] = [];
+
+  x = faX;
+
+  constructor(private dataService: DataService, private formService: FormService) {
+    this.loadItemsList()
+  }
+
+  ngOnInit() {
+    this.subscriptions.add(
+      this.formService
+        .getReloadRequest()
+        .subscribe(async (reloadRequested: boolean) => {
+          if (reloadRequested) {
+            await this.loadItemsList();
+          }
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  async loadItemsList() {
+    let key;
+    let itemsQuery;
+    let invoiceQuery;
+
+    if (this.tableName === 'supplier_invoices') {
+      key = 'total_eur';
+      itemsQuery = 'stocked-items-invoice';
+      invoiceQuery = 'supplier-invoice';
+    } else {
+      key = 'total';
+      itemsQuery = 'invoiced-items';
+      invoiceQuery = 'invoice';
+    }
+
+    this.itemsList = await this.dataService.processGet(
+      itemsQuery,
+      {
+        id: this.invoiceId?.toString(),
+        complex: true,
+      },
+      true
+    );
+
+    const invoiceData = await this.dataService.processGet(invoiceQuery, {
+      filter: this.invoiceId,
+    });
+
+    this.invoiceTotal = invoiceData[key];
+  }
+
+  deleteRow(id: number) {
+    let table = this.tableName == 'supplier_invoices' ? 'stocked_items' : 'invoiced_items';
+
+    this.formService.setSelectedTable(table);
+    this.formService.setDeleteFormIds([id]);
+    this.formService.showDeleteForm();
+  }
+}
