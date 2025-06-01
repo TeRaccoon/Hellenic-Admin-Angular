0e08b04beafff009dbe5f065894560be6e365f29
TeRaccoon <jack.sam.benning@gmail.com>
2025-06-01 11:40:03 +0100

    Added file upload test

--- src/app/components/form/children/file-upload/file-upload.component.spec.ts
+++ src/app/components/form/children/file-upload/file-upload.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FileUploadComponent } from './file-upload.component';
 
@@ -25,5 +26,17 @@ describe('FileUploadComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show warning if file is null', () => {
+    component.file = null;
+
+    fixture.detectChanges();
+
+    const debugLabel = fixture.debugElement.query(By.css('.upload-size-warning'));
+    expect(debugLabel).not.toBeNull();
+
+    const label = debugLabel.nativeElement;
+    expect(label).toBeTruthy();
   });
 });
